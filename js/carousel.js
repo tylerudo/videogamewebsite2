814286d3b61dd536c8c1f85be282f1e72e2e3319
Tyler Udovich <53830171+tylerudo@users.noreply.github.com>
2021-02-01 10:32:06 -0600

    finsihed footer nav and carousel

--- js/carousel.js
+++ js/carousel.js
@@ -14,19 +14,74 @@ const setSlidePosition = (slide, index) => {
 };
 slides.forEach(setSlidePosition);
 
-// when i click left, move slides to the left
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left + ')';   
+    currentSlide.classList.remove('current-slide');
+    targetSlide.classList.add('current-slide');
+}
 
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide');
+targetDot.classList.add('current-slide');
+}
+
+const hideShowArrows = (slides, prevButton, nextButton, targetIndex) => {
+    if (targetIndex === 0) {
+        prevButton.classList.add('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    } else if (targetIndex === slides.length - 1) {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.add('is-hidden');
+    } else {
+        prevButton.classList.remove('is-hidden');
+        nextButton.classList.remove('is-hidden');
+    }
+}
+
+ 
+// when i click left, move slides to the left
+prevButton.addEventListener('click' , e =>{
+    const currentSlide = track.querySelector('.current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.current-slide');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    hideShowArrows(slides, prevButton, nextButton, prevIndex)
+});
 
 // when i click right, move slides to the right
 nextButton.addEventListener('click', e => {
 const currentSlide = track.querySelector('.current-slide');
 const nextSlide = currentSlide.nextElementSibling;
-const amountToMove = nextSlide.style.left;
-    // move to the next slide
-track.style.transform = 'translateX(-' + amountToMove + ')';   
-currentSlide.classList.remove('.current-slide');
-})
+const currentDot = dotsNav.querySelector('.current-slide');
+const nextDot = currentDot.nextElementSibling;
+const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+moveToSlide(track, currentSlide, nextSlide);
+updateDots(currentDot, nextDot);
+hideShowArrows(slides, prevButton, nextButton, nextIndex)
+
+});
 
 
 // when i click the nav idicators, move to that slide
 
+dotsNav.addEventListener('click', e =>{
+    // what indicator was clicked on?
+    const targetDot = e.target.closest('button');
+
+    if (!targetDot)return;
+   
+const currentSlide = track.querySelector('.current-slide');
+const currentDot = dotsNav.querySelector('.current-slide');
+const targetIndex = dots.findIndex(dot => dot === targetDot)
+const targetSlide = slides[targetIndex];
+
+moveToSlide(track, currentSlide, targetSlide);
+updateDots(currentDot, targetDot);
+ hideShowArrows(slides, prevButton, nextButton, targetIndex)
+
+})
